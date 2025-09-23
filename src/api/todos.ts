3965d3f8b8bdb8613e99aebb1335ba79069b1fe1
sajad alizadeh <sajjadalizadeh275800@gmail.com>
2025-09-23 09:22:56 +0330

    added  axios.create

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -2,6 +2,11 @@ import axios from "axios";
 
 const API_URL = "https://dummyjson.com/todos";
 
+const api = axios.create({
+  baseURL: API_URL,
+  timeout: 3000, // 3 seconds
+});
+
 // export interface Todo {
 //   id: number;
 //   todo: string;
@@ -18,24 +23,24 @@ export type Todo = {
 
 // واکشی todos
 export const getTodos = async (limit = 10, skip = 0) => {
-  const res = await axios.get(`${API_URL}?limit=${limit}&skip=${skip}`);
+  const res = await api.get(`${API_URL}?limit=${limit}&skip=${skip}`);
   return res.data; // { todos: Todo[], total, skip, limit }
 };
 
 // حذف todo
 export const deleteTodo = async (id: number) => {
-  const res = await axios.delete(`${API_URL}/${id}`);
+  const res = await api.delete(`${API_URL}/${id}`);
   return res.data;
 };
 
 // تغییر وضعیت completed
 export const toggleComplete = async (id: number, completed: boolean) => {
-  const res = await axios.put(`${API_URL}/${id}`, { completed });
+  const res = await api.put(`${API_URL}/${id}`, { completed });
   return res.data;
 };
 
 // افزودن todo جدید (شبیه‌سازی)
-export const addTodo = async (todo: { todo: string; userId: number }) => {
-  const res = await axios.post(`${API_URL}/add`, todo);
+export const addTodo = async (todo: { todo: string, userId: number, completed: boolean }) => {
+  const res = await api.post(`${API_URL}/add`, todo);
   return res.data;
 };
