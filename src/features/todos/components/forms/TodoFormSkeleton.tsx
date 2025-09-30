672da524ce8c3a0fa5adfd3fe0110d457361abfe
sajad alizadeh <sajjadalizadeh275800@gmail.com>
2025-09-30 12:05:28 +0330

    made TodoFormSkeleton responsive

--- src/features/todos/components/forms/TodoFormSkeleton.tsx
+++ src/features/todos/components/forms/TodoFormSkeleton.tsx
@@ -4,12 +4,23 @@ import Skeleton from "@mui/material/Skeleton";
 export default function TodoFormSkeleton() {
     return (
         <div className="flex flex-col sm:flex-row mb-4 gap-2">
+            {/* Input skeleton */}
             <div className="flex-1 overflow-hidden">
-                <Skeleton variant="rectangular" height={50} animation="wave" style={{ borderRadius: 8 }}
+                <Skeleton
+                    variant="rectangular"
+                    height={50}
+                    animation="wave"
+                    style={{ borderRadius: 8 }}
                 />
             </div>
-            <div className="overflow-hidden">
-                <Skeleton variant="rectangular" width={120} height={50} animation="wave" style={{ borderRadius: 8 }}
+
+            {/* Button skeleton */}
+            <div className="w-full sm:w-[120px] overflow-hidden">
+                <Skeleton
+                    variant="rectangular"
+                    height={50}
+                    animation="wave"
+                    style={{ borderRadius: 8 }}
                 />
             </div>
         </div>
