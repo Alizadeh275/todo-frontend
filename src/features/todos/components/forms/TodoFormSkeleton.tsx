a86f60b53a04c6b24dab1e4b84004373a26f1c68
sajad alizadeh <sajjadalizadeh275800@gmail.com>
2025-10-01 13:16:42 +0330

    added dark mode to the skeletons

--- src/features/todos/components/forms/TodoFormSkeleton.tsx
+++ src/features/todos/components/forms/TodoFormSkeleton.tsx
@@ -1,27 +1,20 @@
 // components/forms/TodoFormSkeleton.tsx
 import Skeleton from "@mui/material/Skeleton";
+import { useTheme } from "@mui/material/styles";
+
+const isDark = document.documentElement.classList.contains("dark");
 
 export default function TodoFormSkeleton() {
+    const theme = useTheme();
+    const skeletonBg = isDark ? theme.palette.grey[800] : theme.palette.grey[300];
+
     return (
         <div className="flex flex-col sm:flex-row mb-4 gap-2">
-            {/* Input skeleton */}
             <div className="flex-1 overflow-hidden">
-                <Skeleton
-                    variant="rectangular"
-                    height={50}
-                    animation="wave"
-                    style={{ borderRadius: 8 }}
-                />
+                <Skeleton variant="rectangular" height={50} animation="wave" sx={{ borderRadius: 2, bgcolor: skeletonBg }} />
             </div>
-
-            {/* Button skeleton */}
             <div className="w-full sm:w-[120px] overflow-hidden">
-                <Skeleton
-                    variant="rectangular"
-                    height={50}
-                    animation="wave"
-                    style={{ borderRadius: 8 }}
-                />
+                <Skeleton variant="rectangular" height={50} animation="wave" sx={{ borderRadius: 2, bgcolor: skeletonBg }} />
             </div>
         </div>
     );
--- src/features/todos/components/items/TodoItemSkeleton.tsx
+++ src/features/todos/components/items/TodoItemSkeleton.tsx
@@ -1,6 +1,12 @@
 import { Skeleton } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+
+const isDark = document.documentElement.classList.contains("dark");
+
 
 export default function TodoItemSkeleton() {
+    const theme = useTheme();
+
     return (
         <div className="flex justify-between items-center p-1 mb-1">
             <Skeleton
@@ -8,7 +14,10 @@ export default function TodoItemSkeleton() {
                 width="100%"
                 height={50}
                 animation="wave"
-                style={{ borderRadius: 8 }}
+                sx={{
+                    borderRadius: 2, // 8px
+                    bgcolor: isDark ? theme.palette.grey[800] : theme.palette.grey[300],
+                }}
             />
         </div>
     );
